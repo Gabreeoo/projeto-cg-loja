c5ba77f7f16718f1c4ecd2f16a6cc3a0276bc49a
PedroVilelaPera <pedro.vpera@gmail.com>
2026-08-31 10:17:23 -0300

    feat: adiciona rota post.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const doces = [
   { id: 1, nome: "Brigadeiro ", preco: 3.5, categoria: "Chocolate" },
   { id: 2, nome: "Bolo", preco: 12.0, categoria: "Bolo" },
@@ -10,6 +12,30 @@ const doces = [
 
 app.get("/api/doces", (req, res) => {
   res.json(doces);
+});
+
+app.post("/api/doces", (req, res) => {
+  const { nome, preco, categoria } = req.body;
+
+  if (!nome || !preco || !categoria) {
+    return res.status(400).json({
+      mensagem: "Todos os campos precisam estar devidamente preenchidos",
+    });
+  } else if (preco <= 0.05) {
+    return res.status(400).json({
+      mensagem: "O produto precisa ter um valor de no mínimo R$ 0.05.",
+    });
+  }
+
+  const novoDoce = {
+    id: doces.length + 1,
+    nome,
+    preco,
+    categoria,
+  };
+
+  doces.push(novoDoce);
+  return res.status(201).json(novoDoce);
 });
 
 app.listen(port, () => {
